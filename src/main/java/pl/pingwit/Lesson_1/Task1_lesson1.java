3fcbef24dd157e46bbc7736d07d20b9b8c1e5fec
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-10 11:18:14 +0300

    task 1 for lesson 2

--- src/main/java/pl/pingwit/Lesson_1/Task1_lesson1.java
+++ src/main/java/pl/pingwit/Lesson_1/Task1_lesson1.java
@@ -1,6 +1,6 @@
 package pl.pingwit.Lesson_1;
 
-public class Task_1 {
+public class Task1_lesson1 {
     public static void main(String[] args) {
         System.out.println("Hello Pingwit, " + args [0]);
     }
