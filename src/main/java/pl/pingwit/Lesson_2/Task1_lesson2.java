611f4a429674ecbd13f3b6f5cbda41f17b76f713
Pavel Radkevich <pradkevich@eisgroup.com>
2023-09-10 15:51:32 +0300

    Проверка 1. Добавил комментарий по неймингу в класс Task1_lesson2

--- src/main/java/pl/pingwit/Lesson_2/Task1_lesson2.java
+++ src/main/java/pl/pingwit/Lesson_2/Task1_lesson2.java
@@ -2,6 +2,13 @@ package pl.pingwit.Lesson_2;
 
 import java.sql.SQLOutput;
 
+// в джаве принято именовать пакеты с маленькой буквы. переименуй, плиз, используя Refactor -> Rename
+// (нужно кликнуть правой клавишей на пакете)
+
+// классы в Джаве принято именовать в Camel case - это значит, без подчеркиваний и начиная каждое новое слово с большой буквы
+// Task1Lesson2
+// плиз переименуй все классы в обеих домашках, используя Refactor -> Rename
+// (нужно кликнуть правой клавишей на классе)
 public class Task1_lesson2 {
     public static void main(String[] args) {
         System.out.println(Short.MIN_VALUE);
