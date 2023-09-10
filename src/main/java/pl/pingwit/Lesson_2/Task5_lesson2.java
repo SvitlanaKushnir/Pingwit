69fa7efd5ce7570b297ff681bb87d37d51c6704f
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-10 12:14:55 +0300

    task 5 for lesson 2

---
+++ src/main/java/pl/pingwit/Lesson_2/Task5_lesson2.java
@@ -0,0 +1,9 @@
+package pl.pingwit.Lesson_2;
+
+public class Task5_lesson2 {
+    public static void main(String[] args) {
+        byte myByte = 7;
+        short myShort = 17000;
+        System.out.println(myShort % myByte);
+    }
+}
