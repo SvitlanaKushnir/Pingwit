362a0de0ee608573d64e31492a9cc6d2e20c3d94
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-10 12:19:17 +0300

    Task 6 for lesson 2

---
+++ src/main/java/pl/pingwit/Lesson_2/Task6_lesson2.java
@@ -0,0 +1,9 @@
+package pl.pingwit.Lesson_2;
+
+public class Task6_lesson2 {
+    public static void main(String[] args) {
+        String myString1 = "7";
+        String myString2 = "7";
+        System.out.println(myString1 + myString2);
+    }
+}
