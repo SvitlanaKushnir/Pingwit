4b2dd9998b4d2f55e7447667a6d98eb0f4f2ffa5
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-10 12:39:38 +0300

    fix мÑ variable values for task 2 lesson 2

--- src/main/java/pl/pingwit/Lesson_2/Task2_lesson2.java
+++ src/main/java/pl/pingwit/Lesson_2/Task2_lesson2.java
@@ -2,19 +2,19 @@ package pl.pingwit.Lesson_2;
 
 public class Task2_lesson2 {
     public static void main(String[] args) {
-        String myString1 = "7";
+        String myString1 = "77";
         Byte myParsedByte = Byte.parseByte(myString1);
         System.out.println(myParsedByte);
 
-        String myString2 = "77";
+        String myString2 = "12587";
         Short myParsedShort = Short.parseShort(myString2);
         System.out.println(myParsedShort);
 
-        String myString3 = "12587";
+        String myString3 = "-78945627";
         Integer myParsedIteger = Integer.parseInt(myString3);
         System.out.println(myParsedIteger);
 
-        String myString4 = "-78945627";
+        String myString4 = "-123.6789";
         Float myParsedFloat = Float.parseFloat(myString4);
         System.out.println(myParsedFloat);
 
