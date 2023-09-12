eb23d679447d7c8546efc242de822607b5cbf5eb
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-13 00:34:05 +0300

    task 1 for lesson 3

--- src/main/java/pl/pingwit/lesson3/Task1Lesso3.java
+++ src/main/java/pl/pingwit/lesson3/Task1Lesso3.java
@@ -3,5 +3,13 @@ package pl.pingwit.lesson3;
 public class Task1Lesso3 {
     public static void main(String[] args) {
 
+        double sum = 10;
+        double result = 10; /* firts day result*/
+        for (int day = 2; day <= 7; day++) {
+            result = result + result * 0.1;
+            sum += result;
+
+        }
+        System.out.println(sum);
     }
 }
