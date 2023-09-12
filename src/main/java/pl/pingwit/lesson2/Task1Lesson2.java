975d2051b0efa2616b179eea9bfa5eb83e2ffaf9
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-12 22:12:51 +0300

    fix task1 for lesson2

--- src/main/java/pl/pingwit/lesson2/Task1Lesson2.java
+++ src/main/java/pl/pingwit/lesson2/Task1Lesson2.java
@@ -1,11 +1,6 @@
 package pl.pingwit.lesson2;
 
-<<<<<<<HEAD:src/main/java/pl/pingwit/lesson2/Task1Lesson2.java
-
 public class Task1Lesson2 {
-=======
-        import java.sql.SQLOutput;
-
 // в джаве принято именовать пакеты с маленькой буквы. переименуй, плиз, используя Refactor -> Rename
 // (нужно кликнуть правой клавишей на пакете)
 
@@ -13,28 +8,23 @@ public class Task1Lesson2 {
 // Task1Lesson2
 // плиз переименуй все классы в обеих домашках, используя Refactor -> Rename
 // (нужно кликнуть правой клавишей на классе)
-    public class Task1_lesson2 {
->>>>>>>611f4a429674ecbd13f3b6f5cbda41f17b76f713:src/main/java/pl/pingwit/Lesson_2/Task1_lesson2.java
+    public static void main(String[] args) {
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
 
-        public static void main(String[] args) {
-            System.out.println(Short.MIN_VALUE);
-            System.out.println(Short.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
 
-            System.out.println(Character.MIN_VALUE);
-            System.out.println(Character.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
 
-            System.out.println(Integer.MIN_VALUE);
-            System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
 
-            System.out.println(Long.MIN_VALUE);
-            System.out.println(Long.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
 
-            System.out.println(Float.MIN_VALUE);
-            System.out.println(Float.MAX_VALUE);
-
-            System.out.println(Double.MIN_VALUE);
-            System.out.println(Double.MAX_VALUE);
-
-
-        }
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
     }
+}
---
+++ src/main/java/pl/pingwit/lesson3/Task1Lesso3.java
@@ -0,0 +1,7 @@
+package pl.pingwit.lesson3;
+
+public class Task1Lesso3 {
+    public static void main(String[] args) {
+
+    }
+}
