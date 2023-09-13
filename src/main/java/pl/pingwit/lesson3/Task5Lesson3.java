f5cb35f8fbc02af1ae523c747da5824cd97e69f7
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-13 21:31:45 +0300

    task 7 for lesson 3

--- src/main/java/pl/pingwit/lesson3/Task5Lesson3.java
+++ src/main/java/pl/pingwit/lesson3/Task5Lesson3.java
@@ -4,8 +4,8 @@ public class Task5Lesson3 {
     public static void main(String[] args) {
         double inch = 2.54;
         double result = 0;
-        for( int i = 1; i <=20; i++) {
-            result += inch ;
+        for (int i = 1; i <= 20; i++) {
+            result += inch;
             System.out.println(result);
         }
 
--- src/main/java/pl/pingwit/lesson3/Task6Lesson3.java
+++ src/main/java/pl/pingwit/lesson3/Task6Lesson3.java
@@ -3,8 +3,8 @@ package pl.pingwit.lesson3;
 public class Task6Lesson3 {
     public static void main(String[] args) {
         int result = 0;
-        for( int i = 2; i <=100; i = i + 2) {
-        result = result + 2;
+        for (int i = 2; i <= 100; i = i + 2) {
+            result = result + 2;
             System.out.println(result);
         }
 
