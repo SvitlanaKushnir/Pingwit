337d2e02fb082312c544f0dcbfc152ae29c6caa8
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-13 21:18:50 +0300

    task 6 for lesson 3

---
+++ src/main/java/pl/pingwit/lesson3/Task6Lesson3.java
@@ -0,0 +1,13 @@
+package pl.pingwit.lesson3;
+
+public class Task6Lesson3 {
+    public static void main(String[] args) {
+        int result = 0;
+        for( int i = 2; i <=100; i = i + 2) {
+        result = result + 2;
+            System.out.println(result);
+        }
+
+    }
+
+}
