6fbfae43ea0f469529438f3aaa8a36eaa09ef1e3
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-14 14:05:38 +0300

    fix task 2

--- src/main/java/pl/pingwit/lesson3/AmoebaCalculator.java
+++ src/main/java/pl/pingwit/lesson3/AmoebaCalculator.java
@@ -1,12 +1,12 @@
 package pl.pingwit.lesson3;
 
 // можно уже выбирать имена, связанные с сутью задачи
-public class Task2Lesson3 {
+public class AmoebaCalculator {
     public static void main(String[] args) {
         int result = 1;
         for (int time = 3; time <= 24; time = time + 3) {
             result = result * 2;
-            System.out.println(result);
+            System.out.println("In " + time + " hours you will see " + result + " amoebas");
             // здесь бы я для читаемости результата предложил выводить так:
             // System.out.println("Через " + time + " часа у вас будет " + result + " амеб");
         }
