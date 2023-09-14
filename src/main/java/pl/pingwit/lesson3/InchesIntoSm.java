7f77a90d3c99bbec47c69407716d10813239cb58
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-14 14:11:47 +0300

    fix task 5

--- src/main/java/pl/pingwit/lesson3/InchesIntoSm.java
+++ src/main/java/pl/pingwit/lesson3/InchesIntoSm.java
@@ -1,13 +1,13 @@
 package pl.pingwit.lesson3;
 
-public class Task5Lesson3 {
+public class InchesIntoSm {
     public static void main(String[] args) {
         double inch = 2.54;
         double result = 0;
         for (int i = 1; i <= 20; i++) {
             result += inch;
             // здесь бы хотелось видеть пояснения, сколько дюймов и сколько см
-            System.out.println(result);
+            System.out.println( i + " inch = " + result + " sm");
         }
 
     }
