0cab43d5edb9dedf12cba242f848c5e531afca13
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-14 22:03:12 +0300

    add constant

--- src/main/java/pl/pingwit/lesson3/RunningDistanceCalculator.java
+++ src/main/java/pl/pingwit/lesson3/RunningDistanceCalculator.java
@@ -2,10 +2,14 @@ package pl.pingwit.lesson3;
 
 // поправь плиз имя. можно уже выбирать имена, связанные с сутью задачи: например RunningDistanceCalculator
 public class RunningDistanceCalculator {
+
+
+    public static final int FIRST_DAY_RESULT = 10;
+
     public static void main(String[] args) {
 
-        double sum = 10;
-        double result = 10; /* firts day result*/ // предлагаю вынести в константу и использовать в строках 7 и 8
+        double sum = FIRST_DAY_RESULT;
+        double result = FIRST_DAY_RESULT;
         for (int day = 2; day <= 7; day++) {
             result = result + result * 0.1;
             sum += result;
