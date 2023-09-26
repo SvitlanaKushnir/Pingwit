db16d56320f2872961f8d577003f88e468db00b7
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-26 14:09:28 +0300

    task 5 for lesson 5

---
+++ src/main/java/pl/pingwit/lesson5/arrayReverse.java
@@ -0,0 +1,11 @@
+package pl.pingwit.lesson5;
+
+import java.util.Arrays;
+
+public class arrayReverse {
+    public static void main(String[] args) {
+        int myArray[] = {1, 2, 3, 4, 5};
+        for (int i = myArray.length - 1;i >= 0; i--)
+            System.out.println(myArray[i]);
+        }
+}
