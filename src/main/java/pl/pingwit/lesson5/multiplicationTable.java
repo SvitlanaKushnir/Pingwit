c3e89843bd31eddc4ea58e7d5b8cd425a97e27ae
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-09-26 16:10:24 +0300

    task 4 for lesson 5

---
+++ src/main/java/pl/pingwit/lesson5/multiplicationTable.java
@@ -0,0 +1,13 @@
+package pl.pingwit.lesson5;
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+        int[][] myTwoArrays = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
+        for (int i = 0; i < myTwoArrays[0].length; i++) {
+            for (int j = 0; j < myTwoArrays[1].length; j++) {
+                int multiplication = myTwoArrays[0][i] * myTwoArrays[1][j];
+                System.out.println(myTwoArrays[0][i] + " * " + myTwoArrays[1][j] + " = " + multiplication);
+            }
+        }
+    }
+}
