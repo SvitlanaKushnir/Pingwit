f965781a90fd5b370d40fa2829983e66207a7f83
ИиSvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-10-02 16:37:16 +0300

    fix for task 1 lesson 6

--- src/main/java/pl/pingwit/lesson6/BubbleSort.java
+++ src/main/java/pl/pingwit/lesson6/BubbleSort.java
@@ -5,14 +5,13 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] myArray = {2, 1, 3, 5, 4};
+
         int n;
-        for (int i = 0; i < myArray.length; i++) {
-            for (int j = 0; j < myArray.length - 1; j++) {
-                if (myArray[j] > myArray[j + 1]) {
-                    n = myArray[j];
-                    myArray[j] = myArray[j + 1];
-                    myArray[j + 1] = n;
-                }
+        for (int i = 0; i < myArray.length - 1; i++) {
+            if (myArray[i] > myArray[i + 1]) {
+                n = myArray[i];
+                myArray[i] = myArray[i + 1];
+                myArray[i + 1] = n;
             }
         }
         System.out.println(Arrays.toString(myArray));
