fbd24494b8c6fb44341cc88463852c490542cd09
Pavel Radkevich <pradkevich@eisgroup.com>
2023-10-25 10:03:18 +0300

    ДЗ10,  проверка 1

--- src/main/java/pl/pingwit/lesson10/task2/PalindromArray.java
+++ src/main/java/pl/pingwit/lesson10/task2/PalindromArray.java
@@ -3,6 +3,10 @@ package pl.pingwit.lesson10.task2;
 import java.util.Arrays;
 
 public class PalindromArray {
+    /*
+    классы PalindromArray и StringUtilsPalindrome делаю по сути то же самое (checkPalindrome()) , но разными способами.
+    имена у них должны быть подобными: ...PalindromeChecker, например ArrayPalindromeChecker и StringUtilsPalindromeChecker
+     */
     public boolean checkPalindrom(String word) {
         char[] wordArray = word.toCharArray();
         char[] reversedWordArray = new char[wordArray.length];
