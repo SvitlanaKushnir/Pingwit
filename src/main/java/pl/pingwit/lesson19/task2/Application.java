57c32a280cbce535379f78ecaa724dd6013c7d4f
SvitlanaKushnir <svitlana.kushnir@pragmaticplay.com>
2023-12-04 11:53:51 +0200

    task 2 for lesson 19

---
+++ src/main/java/pl/pingwit/lesson19/task2/Application.java
@@ -0,0 +1,15 @@
+package pl.pingwit.lesson19.task2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        ATM[] atm=objectMapper.readValue(new File("src/main/java/pl/pingwit/lesson19/task2/atms.json"),ATM[].class);
+        System.out.println(Arrays.toString(atm));
+    }
+}
